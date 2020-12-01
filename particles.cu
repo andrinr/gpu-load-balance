04bce164cae9b4d8b74367ed666ce3ba4ca77e3b
Andrin Rehmann <andrinrehmann@gmail.com>
2020-12-01 11:25:29 +0100

    sucessfully added output data

--- particles.cu
+++ particles.cu
@@ -1,46 +1,61 @@
 #include "math.h"
 #include "stdio.h"
+#include <experimental/random>
 #include <iostream>
 #include <fstream>
 
-__global__ void step(int N, double *pos, double *vel, double h){
-	int index = threadIdx.x;
+__global__ void split(int N, int * pos){
 	int stride = blockDim.x;
-}	
-
-__global__ void split(int N, double *xpos){
-	// smt
+	int index = threadIdx.x;
 }
 
 int main() 
 {
-	int N = 10<<20; // 1M Elemets
-	double *xpos;
-	
+	int N = 10<<16; // 60k  Elemets
+	int *xpos;
+	int *domain;
+
 	cudaMallocManaged(&xpos, N*sizeof(double));
+	cudaMallocManaged(&domain, N*sizeof(int));
 	
-	double pos [N*3];
-	double vel [N*3];
+	int pos [N*3];
+	int vel [N*3];
 
 	for (int i = 0; i < N; i++){
-		pos[3*i] = std::rand();
-		pos[3*i+1] = std::rand();
-		pos[3*i+2] = std::rand();
+		// xpos
+		pos[3*i] = std::experimental::randint(INT_MIN, INT_MAX);
+		xpos[i] = pos[3*i];
+		domain[0] = 0;
+		// ypos
+		pos[3*i + 1] = std::experimental::randint(INT_MIN, INT_MAX);
+		// zpos
+		pos[3*i + 2] = std::experimental::randint(INT_MIN, INT_MAX);
 
+		// xvel
 		vel[3*i] = 0;
+		// yvel
 		vel[3*i+1] = 0;
+		// zpos
 		vel[3*i+2] = 0;
 	}
-
 	
-	// :step<<<1,256>>>(N, pos, vel, 0.001); 
+	
+	split<<<1,256>>>(N, xpos); 
 
 	cudaDeviceSynchronize();
 	
 	printf("calculated one step");
 
-	cudaFree(pos);
-	cudaFree(vel);
+	cudaFree(xpos);
+	
+	remove( "out.dat" );
+	std::ofstream Data("out.dat");
+	
+	for (int i = 0; i < N; i++){
+		Data << pos[3*i] << " " << pos[3*i +1] << " " << pos[3*i+2] << "\n";
+	}
+
+	Data.close();
     	return 0;
 }
 
