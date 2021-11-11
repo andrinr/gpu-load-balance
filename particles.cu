57c9d9d82f6291638090b7052dd087d46ee1e6ec
andrinr <andrinrehmann@gmail.com>
2021-11-11 08:57:47 +0100

    parallel summation

--- particles.cu
+++ particles.cu
@@ -5,7 +5,12 @@
 #include <fstream>
 #include <assert.h>
 
-__global__ void split(int nPositions, int * positions, int splitPosition, unsigned int * splitSizes){
+__global__ void split(
+	int nPositions, 
+	int * positions, 
+	int splitPosition, 
+	unsigned int * splitSizes
+){
 	
 	unsigned int index = threadIdx.x + blockIdx.x * blockDim.x;
 
@@ -20,7 +25,8 @@ __global__ void split(int nPositions, int * positions, int splitPosition, unsign
 	// sequential reduction, can be optimized further
         for (unsigned int s = blockDim.x/2; s > 0; s >>= 1){
                 if (threadIdx.x < s){
-			s_splitSizes[threadIdx.x] += s_splitSizes[threadIdx.x + s];
+			s_splitSizes[threadIdx.x] 
+				+= s_splitSizes[threadIdx.x + s];
 		}
 		__syncthreads();
 	}
@@ -41,8 +47,16 @@ __global__ void sum(int size, unsigned int*values){
 
 }
 
-__global__ void findDomainID(int nPositions, int * positions, int splitPosition, unsigned int * domainIDs){
-	for (unsigned int i = threadIdx.x; i < nPositions; i += blockDim.x){
+__global__ void findDomainID(
+	int nPositions, 
+	int * positions, 
+	int splitPosition, 
+	unsigned int * domainIDs
+){
+	for (
+		unsigned int i = threadIdx.x;
+		i < nPositions; i += blockDim.x
+	){
 		int isLeft = positions[i] < splitPosition;
 		domainIDs[i] = isLeft;
 	}
@@ -56,11 +70,14 @@ int main()
 
 	// Memory size
 	int size = N * sizeof(int);
-	
+	int unsignedSize = N * sizeof(unsigned int);
+
 	// Host memory
 	int* h_xPos = (int*)malloc(size);
 	int* h_yPos = (int*)malloc(size);
 	int* h_zPos = (int*)malloc(size);
+	
+	unsigned int* h_domainID = (unsigned int*)malloc(unsignedSize);
 
 	// Device memory
 	int* d_xPos;
@@ -71,6 +88,9 @@ int main()
 
 	int* d_zPos;
 	cudaMalloc(&d_zPos, size);
+
+	unsigned int* d_domainID;
+	cudaMalloc(&d_domainID, unsignedSize);
 
 	// Data Initialisation
 	for (int i = 0; i < N; i++){
@@ -95,52 +115,93 @@ int main()
 
 	unsigned int h_splitSize;
 
-	unsigned int* h_splitSizes = (unsigned int*)malloc(nBlocks*sizeof(unsigned int));
+	unsigned int* h_splitSizes = 
+		(unsigned int*)malloc(nBlocks*sizeof(unsigned int));
+	
 	unsigned int* d_splitSizes;
 	cudaMalloc(&d_splitSizes, nBlocks * sizeof(unsigned int));
 
 	// Binary search for ideal splitPosition, assuming 32bit integers
 	for (int i = 30; i >= 0; i--){
-		split<<<nBlocks, nThreads, nThreads*sizeof(unsigned int)>>>(N, d_xPos, splitPosition, d_splitSizes);
-
-		cudaMemcpy(h_splitSizes, d_splitSizes, nBlocks*sizeof(unsigned int), cudaMemcpyDeviceToHost);
-
+		
+		// find split
+		split<<<
+			nBlocks, 
+			nThreads, 
+			nThreads*sizeof(unsigned int)
+		>>>(
+			N, 
+			d_xPos, 
+			splitPosition, 
+			d_splitSizes
+		);
+		
+		// copy back memory from device to host
+		cudaMemcpy(
+			h_splitSizes, 
+			d_splitSizes, 
+			nBlocks*sizeof(unsigned int), 
+			cudaMemcpyDeviceToHost
+		);
+		
+		// Sum up splitSizes from blocks
 		h_splitSize = 0;
 		for (unsigned int j = 0; j < nBlocks; j++){
 			h_splitSize += h_splitSizes[j];	
 		}
 
-		// Sum up results from blocks
+		// or also do it on GPU
 		//sum<<<1, nThreads>>>(nBlocks, d_splitSizes);
 		// Copy back to host
 		//cudaMemcpy(&h_splitSize, d_splitSizes, sizeof(unsigned int), cudaMemcpyDeviceToHost);
 
-		std::cout << splitPosition << " : " <<  h_splitSize << "\n";	
+		std::cout 
+			<< splitPosition << " : " 
+			<<  h_splitSize << "\n";	
+		
 		if (h_splitSize > 1<<(p-1)){
 			splitPosition -= 1 << i;
 		}
 		else if(h_splitSize < 1<<(p-1)){
 			splitPosition += 1 << i;
 		}
+		// ideal split has been found
 		else{
-			std::cout << "found split at position " << splitPosition << "\n";
+			std::cout 
+				<< "found split at position " 
+				<< splitPosition << "\n";
 			break;
 		}
-	}	
- 	
+	}
+ 
+	findDomainID
+		<<<nBlocks, nThreads, nThreads*sizeof(unsigned int)>>>
+ 		(N, d_xPos, splitPosition, h_domainID);	
+	
+	cudaMemcpy(
+		h_domainID, 
+		d_domainID, 
+		nBlocks*sizeof(unsigned int), 
+		cudaMemcpyDeviceToHost
+	);
+
 	// Free memory
 	cudaFree(d_xPos);
 	cudaFree(d_splitSizes);
 
 	// Output
 	remove( "out.dat" );
-	//std::ofstream Data("out.dat");
+	std::ofstream Data("out.dat");
 	
-	//for (int i = 0; i < N; i++){
-	//	Data << h_xPos[3*i] << " " << h_yPos[3*i +1] << " " << h_domain[i]  << "\n";
-	//}
+	for (int i = 0; i < N; i++){
+		Data 
+			<< h_xPos[3*i] << " " 
+			<< h_yPos[3*i +1] << " " 
+			<< h_domainID[i]  << "\n";
+	}
 
-	//Data.close();
+	Data.close();
     	return 0;
 }
+
 
