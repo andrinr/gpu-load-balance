57df80f31b453d76a3500ae41f98fcad213dde54
Andrin Rehmann <andrinrehmann@gmail.com>
2022-08-02 19:00:39 +0200

    fix errors in script

--- analysis/analysis.job.sh
+++ analysis/analysis.job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
-#SBATCH --job-name="cpuReduce-7" 
+#SBATCH --job-name="orbit-1-30" 
 #SBATCH --account=uzg2
 #SBATCH --constraint=gpu
 #SBATCH --time=0-0:01:00
 #SBATCH --partition=debug
 #SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=12 --ntasks-per-core=1
 
-pParticles=128
-pDomains=27
-opt=0
-outFile="results0"
+pParticles=30
+pDomains=10
+opt=1
+outFile="results1"
 srun ../build/orbit $pParticles $pDomains $opt >>$outFile
--- analysis/analysis.sh
+++ analysis/analysis.sh
@@ -1,25 +1,25 @@
 
 rm *.out
-rm results
+rm results*
 
 # No optimizatoin
-for j in 25; do
+for j in 30; do
     sed -i  "s/job-name=.*$/job-name=\"orbit-0-$j\" /g" analysis.job.sh;
-    sed -i "s/nParticles=.*$/nParticles=j/g" analysis.job.sh;
-    sed -i "s/pDomains=.*$/nParticles=10/g" analysis.job.sh;
-    sed -i "s/opt=.*$/nParticles=0/g" analysis.job.sh;
-    ed -i "s/outFile=.*$/outFile=\"results0\"/g" analysis.job.sh;
+    sed -i "s/pParticles=.*$/pParticles=$j/g" analysis.job.sh;
+    sed -i "s/pDomains=.*$/pDomains=10/g" analysis.job.sh;
+    sed -i "s/opt=.*$/opt=0/g" analysis.job.sh;
+    sed -i "s/outFile=.*$/outFile=\"results0\"/g" analysis.job.sh;
     # run script
     sbatch --wait analysis.job.sh
 done
 
 
 # No optimizatoin
-for j in 25; do
+for j in 30; do
     sed -i  "s/job-name=.*$/job-name=\"orbit-1-$j\" /g" analysis.job.sh;
-    sed -i "s/nParticles=.*$/nParticles=j/g" analysis.job.sh;
-    sed -i "s/pDomains=.*$/nParticles=10/g" analysis.job.sh;
-    sed -i "s/opt=.*$/nParticles=1/g" analysis.job.sh;
+    sed -i "s/pParticles=.*$/pParticles=$j/g" analysis.job.sh;
+    sed -i "s/pDomains=.*$/pDomains=10/g" analysis.job.sh;
+    sed -i "s/opt=.*$/opt=1/g" analysis.job.sh;
     sed -i "s/outFile=.*$/outFile=\"results1\"/g" analysis.job.sh;
     # run script
     sbatch --wait analysis.job.sh
