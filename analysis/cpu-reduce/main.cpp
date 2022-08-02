dc5170aad4520a2fdb7566328cefc8c4fb83b96c
andrinr <andrinrehmann@gmail.com>
2022-08-02 12:48:39 +0200

    impr cpu reduce stand alone

--- analysis/cpu-reduce/main.cpp
+++ analysis/cpu-reduce/main.cpp
@@ -5,28 +5,28 @@
 #include <blitz/array.h>
 #include <chrono>
 #include <iostream>
+#include <thread>
+#include <vector>
 
 int sum(blitz::Array<float, 2> particles, int n, int k) {
 
     int nLeft = 0;
     float cut = 0.5;
 
-    float * startPtr = particles.data();
+    float * startPtr = particles.data() + k;
     float * endPtr = startPtr + n;
-
-    auto start = std::chrono::high_resolution_clock::now();
 
     // We use pointers to iterate over the particles
     for(auto p= startPtr; p<endPtr; ++p) nLeft += *p < cut;
 
-    auto end = std::chrono::high_resolution_clock::now();
 
-    std::cout << "cost in microseconds with c style pointer iteration " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "\n";
+
     return nLeft;
 }
 
 int main(int argc, char** argv) {
-    int n = 1 << 27;
+    
+    int n = 1 << strtol(argv[1], nullptr, 0);;
     int k = 10;
     std::cout << "Performing measurements " << n << "\n";
 
@@ -41,9 +41,25 @@ int main(int argc, char** argv) {
     float * startPtr = particles.data();
     float * endPtr = startPtr + n * k;
 
-    for(auto p= startPtr; p<endPtr; ++p) *p = (float)(rand())/(float)(RAND_MAX);;
+    //for(auto p= startPtr; p<endPtr; ++p) *p = (float)(rand())/(float)(RAND_MAX);;
 
-    sum(particles, n , k);
+    int nThreads = strtol(argv[2], nullptr, 0);;
+    std::vector<std::thread> threads;
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < nThreads; i++) {
+        threads.push_back(std::thread(sum, particles, n / nThreads , n / nThreads * i));
+    }
+
+    for (int i = 0; i < nThreads; i++) {
+        threads[i].join();
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::cout << "cost in microseconds with c style pointer iteration " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "\n";
+
 
     return 0;
 }
