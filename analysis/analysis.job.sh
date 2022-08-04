40329011ba42a9ddd89cd5733bcce5897e84bb60
Andrin Rehmann <andrinrehmann@gmail.com>
2022-08-04 10:12:02 +0200

    adapt script

--- analysis/analysis.job.sh
+++ analysis/analysis.job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
-#SBATCH --job-name="orbit-1-30" 
+#SBATCH --job-name="orbit-1-28" 
 #SBATCH --account=uzg2
 #SBATCH --constraint=gpu
 #SBATCH --time=0-0:30:00
 #SBATCH --partition=debug
 #SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=12 --ntasks-per-core=1
 
-pParticles=30
+pParticles=28
 pDomains=10
 opt=1
 outFile="results1"
