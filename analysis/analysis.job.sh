44a59c2ee72baf0d0bf1acf8959768cd813cb3d4
Andrin Rehmann <andrinrehmann@gmail.com>
2022-08-04 10:44:25 +0200

    add measurements

--- analysis/analysis.job.sh
+++ analysis/analysis.job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
-#SBATCH --job-name="orbit-1-28" 
+#SBATCH --job-name="orbit-1-30" 
 #SBATCH --account=uzg2
 #SBATCH --constraint=gpu
 #SBATCH --time=0-0:30:00
 #SBATCH --partition=debug
 #SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=12 --ntasks-per-core=1
 
-pParticles=28
+pParticles=30
 pDomains=10
 opt=1
 outFile="results1"
